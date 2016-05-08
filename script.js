b437352ec57314e1ab08b013e1c7ffc769c044eb
Lindsay Reynolds <reynolds.lk@outlook.com>
2016-05-08 00:09:54 -0700

    Added content to main page, added tiny JavaScript file

---
+++ script.js
@@ -0,0 +1,14 @@
+// Outputs an alert box with "ok" button
+alert("Hello world!");
+
+// Create variable for holding user input, get user input
+var name = prompt("What is your name?");
+
+// Print out name
+alert("Hello, " + name + "!");
+
+//Prompt for a string
+name = prompt("Please enter a word.");
+
+//Get length of string
+alert(name.length);
